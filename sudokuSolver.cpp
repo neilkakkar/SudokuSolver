2fc81ddedea98894cf1993db330c4c608342031a
neilkakkar <neilkakkar@gmail.com>
2016-10-05 15:23:12 +0530

    created header and cpp file

---
+++ sudokuSolver.cpp
@@ -0,0 +1,12 @@
+
+
+vector<int> possibleNums(vector<vector<int> >& board, int i,int j)
+{
+    vector<int> ans;
+    int poss[10];
+    for(int k=0;k<10;k++)poss[k]=0;
+    //check column
+    for(int k=0;k<N;k++){
+
+    }
+}
---
+++ sudokuSolver.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 9;
+
+vector<int> possibleNums(vector<vector<int> >& board,int i,int j);
+
+bool sudokuSolution(vector<vector<int> >& board);
