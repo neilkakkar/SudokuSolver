5c7bab07a7dd54f80443cdcffd183a19fda5d013
neilkakkar <neilkakkar@gmail.com>
2016-10-05 15:51:05 +0530

    completed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include "sudokuSolver.h"
 
 int main(){
+    vector<vector<int> > board;
+    vector<int> X(N);
+    for(int i=0;i<N;i++)board.push_back(X);
 
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cin>>board[i][j];
+        }
+    }
+    if(sudokuSolution(board)){
+        cout<<"Solution: \n";
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cout<<board[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    else{
+        cout<<"No solution exists.";
+    }
 }
