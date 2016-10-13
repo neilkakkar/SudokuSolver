940d91b6dc978c19b3ec87703b75a8d8aec42c09
Neil Kakkar <neilkakkar@gmail.com>
2016-10-13 18:40:29 +0530

    Update main.cpp
    Added comments.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@ int main(){
     vector<vector<int> > board;
     vector<int> X(N);
     for(int i=0;i<N;i++)board.push_back(X);
-
+    
+    // Take the board as input. 1-9 as given original numbers, and empty spaces represented by 0.
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             cin>>board[i][j];
         }
     }
+    // Output solution if it exists
     if(sudokuSolution(board)){
         cout<<"Solution: \n";
         for(int i=0;i<N;i++){
